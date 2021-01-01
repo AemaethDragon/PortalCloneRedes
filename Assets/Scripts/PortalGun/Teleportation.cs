f10de0f9dcb1a551fab78fb5a01e1a4c44577d32
AemaethDragon <58938365+AemaethDragon@users.noreply.github.com>
2021-01-01 04:21:17 +0000

    Fixed Portals Teleportation
    Coisas estranahas foram adicionadas nas novas versoes do Unity que dificultou a criaçao de scripts para teleport

--- Assets/Scripts/PortalGun/Teleportation.cs
+++ Assets/Scripts/PortalGun/Teleportation.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class Teleportation : MonoBehaviour
 {
     #region variables
-    private Transform goTo;
-    
-    public GameObject player;
+    private Transform _goTo;
+    private GameObject _tempPortal;
     #endregion
 
     #region methods
@@ -15,21 +14,20 @@ public class Teleportation : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Player") && gameObject.CompareTag("PortalLeft"))
         {
-            Debug.Log("ENTERED");
-            goTo = GameObject.FindGameObjectWithTag("PortalRight").GetComponent<Transform>();
+            _tempPortal = GameObject.FindGameObjectWithTag("PortalRight");
+            _goTo = _tempPortal.transform;
+            col.transform.position = new Vector3(_goTo.transform.position.x,
+                _goTo.transform.position.y, _goTo.transform.position.z + 2);
+            col.transform.Rotate(Vector3.up, 180f);
         }
         else if (col.gameObject.CompareTag("Player") && gameObject.CompareTag("PortalRight"))
         {
-            Debug.Log("ENTERED2");
-            goTo = GameObject.FindGameObjectWithTag("PortalLeft").GetComponent<Transform>();
-        }
-        
-        if (Vector3.Distance(player.transform.position, col.transform.position) < 0.2f)
-        {
-            col.transform.position = new Vector3(goTo.position.x, goTo.position.y, goTo.position.z);
-                               
+            _tempPortal = GameObject.FindGameObjectWithTag("PortalLeft");
+            _goTo = _tempPortal.transform;
+            col.transform.position = new Vector3(_goTo.transform.position.x,
+                _goTo.transform.position.y, _goTo.transform.position.z - 2);
             col.transform.Rotate(Vector3.up, 180f);
-        }                
+        }
     }
     #endregion
 }
