c6ca1e08e32688fd3a86cfab89317dfebb95b4bf
AemaethDragon <58938365+AemaethDragon@users.noreply.github.com>
2021-01-01 00:28:08 +0000

    stuff

--- Assets/Scripts/PortalGun/Teleportation.cs
+++ Assets/Scripts/PortalGun/Teleportation.cs
@@ -5,28 +5,26 @@ using UnityEngine;
 public class Teleportation : MonoBehaviour
 {
     #region variables
-    
     private Transform goTo;
+    
     public GameObject player;
-
-    public bool isLeft;
     #endregion
 
     #region methods
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.CompareTag("Player") && this.gameObject.CompareTag("PortalLeft"))
+        if (col.gameObject.CompareTag("Player") && gameObject.CompareTag("PortalLeft"))
         {
             Debug.Log("ENTERED");
             goTo = GameObject.FindGameObjectWithTag("PortalRight").GetComponent<Transform>();
         }
-        else if (col.gameObject.CompareTag("Player") && this.gameObject.CompareTag("PortalRight"))
+        else if (col.gameObject.CompareTag("Player") && gameObject.CompareTag("PortalRight"))
         {
             Debug.Log("ENTERED2");
             goTo = GameObject.FindGameObjectWithTag("PortalLeft").GetComponent<Transform>();
         }
         
-        if (Vector3.Distance(transform.position, col.transform.position) < 0.2f)
+        if (Vector3.Distance(player.transform.position, col.transform.position) < 0.2f)
         {
             col.transform.position = new Vector3(goTo.position.x, goTo.position.y, goTo.position.z);
                                
