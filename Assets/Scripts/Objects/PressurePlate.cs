e1e333c3b70220ff93b9ea0055d6ebe4a21e1017
SrCopes <filipe5lopes@hotmail.com>
2021-01-16 16:21:17 +0000

    PressurePlate semii done
    semi done

---
+++ Assets/Scripts/Objects/PressurePlate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pressurePlate;
+
+    bool isPress = false;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (!isPress)
+        {
+            isPress = true;
+            pressurePlate.transform.position += new Vector3(0.15f, 12.26f, -6.316681f);
+        }
+    }
+}
