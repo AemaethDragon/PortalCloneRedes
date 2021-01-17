3ceabf5bd5d0ba2c01167070577d539f85b94eaf
Helder Silva <a13009@alunos.ipca.pt>
2021-01-17 01:44:41 +0000

    Half Assed Working Multiplayer

--- Assets/Scripts/RigidBodyController/PlayerMovement.cs
+++ Assets/Scripts/RigidBodyController/PlayerMovement.cs
@@ -1,39 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Common;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerController playerController;
     public Rigidbody rb;
     public Transform _camera;
-
-    public float moveSpeed = 4f;
-    public float jumpForce = 6f;
-
     public LayerMask Ground;
-
     public bool isGrounded;
+
+    private float _jumpForce = 4f;
+    private float _moveSpeed = 4f;
 
     void Update()
     {
+       if (!playerController.Playable) return;
+        
         //grounding
         isGrounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), 1f, Ground);
-
-
+        
         //facing direction
         Debug.DrawLine(_camera.position, transform.forward * 2.5f);
-
+        
         //moving
-        float x = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        float y = Input.GetAxisRaw("Vertical") * moveSpeed;
-
+        float x = Input.GetAxisRaw("Horizontal") *  _moveSpeed;
+        float y = Input.GetAxisRaw("Vertical") *  _moveSpeed;
+        
         //jumping
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) 
+            rb.velocity = new Vector3(rb.velocity.x, _jumpForce, rb.velocity.z);
 
         //setting movement
         Vector3 move = transform.right * x + transform.forward * y;
-
         rb.velocity = new Vector3(move.x, rb.velocity.y, move.z);
     }
 }
--- Assets/Scripts/TCP/PlayerController.cs
+++ Assets/Scripts/TCP/PlayerController.cs
@@ -1,28 +1,21 @@
-﻿using Common;
+﻿using System;
+using Common;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-
+public class PlayerController : MonoBehaviour
+{
     public bool Playable;
-    private Vector3 _oldPosition;
     private float _horizontal;
     private float _vertical;
+    private Vector3 _oldPosition;
+    private float _moveSpeed = 4f;
     public TcpClientController TcpClient;
-    void Update () {
-        if (!Playable) return;
-
-        _horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-        _vertical = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-	}
-
+    
     void FixedUpdate()
     {
         if (!Playable) return;
-
-        transform.Translate(_horizontal, 0, _vertical);
         if (transform.position != _oldPosition)
         {
             Message m = new Message();
